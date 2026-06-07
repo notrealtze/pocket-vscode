5d6c92b28f21bdba752ffcd085cf074f9cfb0082
notrealtze <you@example.com>
2026-06-07 16:51:37 -0500

    Keep system nav bar visible, block fullscreen

--- app/src/main/java/com/pocketvscode/MainActivity.kt
+++ app/src/main/java/com/pocketvscode/MainActivity.kt
@@ -2,6 +2,8 @@ package com.pocketvscode
 
 import android.os.Bundle
 import android.view.View
+import android.view.WindowInsets
+import android.view.WindowInsetsController
 import android.webkit.*
 import android.widget.Button
 import android.widget.TextView
@@ -23,6 +25,27 @@ class MainActivity : AppCompatActivity() {
         loadingText.visibility = View.GONE
         webView.visibility = View.VISIBLE
         webView.loadUrl(VSCODE_URL)
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) showSystemUI()
+    }
+
+    private fun showSystemUI() {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R) {
+            window.insetsController?.let {
+                it.show(WindowInsets.Type.systemBars())
+                it.systemBarsBehavior = WindowInsetsController.BEHAVIOR_DEFAULT
+            }
+        } else {
+            @Suppress("DEPRECATION")
+            window.decorView.systemUiVisibility = (
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            )
+        }
     }
 
     private fun setupZoomButtons() {
@@ -55,7 +78,12 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
-        webView.webChromeClient = WebChromeClient()
+        webView.webChromeClient = object : WebChromeClient() {
+            override fun onShowCustomView(view: View, callback: CustomViewCallback) {
+                // Block fullscreen requests from VSCode
+                callback.onCustomViewHidden()
+            }
+        }
         webView.addJavascriptInterface(StorageBridge(this), "AndroidStorage")
     }
 }
